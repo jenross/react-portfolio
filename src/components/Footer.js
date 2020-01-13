43b9b0f4b7bef98574e695e3d0f075c483e6797d
jenross <ross.m.jennifer@gmail.com>
2020-01-13 07:27:08 -0500

    updated footer year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -48,7 +48,7 @@ const Footer = () => (
           <FooterContainer>
             <Fade left>
               <TextFooter fontSize={[1, 1]}>
-                <span>{`© 2019 - Powered by `}</span>
+                <span>{`© 2020 - Powered by `}</span>
                 <Link href="https://www.gatsbyjs.org/">Gatsby</Link>
                 <span> and </span>
                 <Link href="https://www.contentful.com/" mr={1}>
